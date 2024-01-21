6714ca5b6fee07db076a9da3fefd4cb7b2178db7
Krishan Shamod <krishanshamod@gmail.com>
2024-01-21 14:29:00 +0530

    Updated bash script

--- .github/workflows/scripts/increment-version.sh
+++ .github/workflows/scripts/increment-version.sh
@@ -11,7 +11,7 @@ do
 done
 
 # get highest tag number, and add 1.0.0 if doesn't exist
-CURRENT_VERSION=`git describe --abbrev=0 --always --tags 2>/dev/null`
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
